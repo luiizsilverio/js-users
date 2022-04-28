426331a8d8c1961ed6d079cc6c2f6f36e8d12c27
luiizsilverio <luiiz.silverio@gmail.com>
2022-04-27 21:06:46 -0300

    fileReader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,7 +15,11 @@ class UserController {
         if (field.checked) {
           userData[field.name] = field.value
         }
-      } else {
+      }
+      else if (field.name === "admin") {
+        userdata[field.name] = field.checked
+      }
+      else {
         userData[field.name] = field.value
       }
     })
@@ -36,28 +40,58 @@ class UserController {
 
 
   onSubmit() {
-
     this.formEl.addEventListener("submit", (event) => {
 
       event.preventDefault()
 
       let user = this.getValues()
 
-      this.addUser(user, this.tableId)
+      this.getImage()
+        .then((content) => {
+          user.photo = content
+          this.addUser(user, this.tableId)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
 
     })
   }
 
+  getImage() {
+    return new Promise((resolve, reject) => {
+
+      let fileReader = new FileReader()
+
+      let elFoto = Array.from(this.formEl.elements).filter(item => item.name === 'photo')
+
+      let file = elFoto[0].files[0]
+
+      fileReader.onload = () => {
+        resolve(fileReader.result) // retorna a URL do arquivo
+      }
+
+      fileReader.onerror = (ev) => {
+        reject(ev)
+      }
+
+      if (file) {
+        fileReader.readAsDataURL(file)
+      } else {
+        resolve('dist/img/boxed-bg.jpg') // retorna foto padrão
+      }
+    })
+  }
 
   addUser(user) {
     let trUser = document.createElement('tr')
 
     trUser.innerHTML = `
       <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td><img src="${user.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${user.name}</td>
         <td>${user.email}</td>
-        <td>${user.admin}</td>
+        <td>${user.admin ? "Sim" : "Não"}</td>
         <td>${user.birth}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
@@ -65,6 +99,7 @@ class UserController {
         </td>
       </tr>
     `
+
     this.tableEl.append(trUser)
   }
 }
