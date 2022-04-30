228d451418881816c04d4e273bf14291b53a7b91
luiizsilverio <luiiz.silverio@gmail.com>
2022-04-30 14:59:53 -0300

    Editar usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,8 @@ class UserController {
   constructor(formId, tableId) {
     this.formEl = document.getElementById(formId)
     this.tableEl = document.getElementById(tableId)
+    this.userSel = {}
+    this.userIndex = -1
     this.onSubmit()
   }
 
@@ -14,7 +16,11 @@ class UserController {
 
       if (field.name === "gender") {
         if (field.checked) {
-          userData[field.name] = field.value
+          if (field.id === "exampleInputGenderM") {
+            userData[field.name] = "M"
+          } else {
+            userData[field.name] = "F"
+          }
         }
       }
       else if (field.name === "admin") {
@@ -39,6 +45,32 @@ class UserController {
     return objUser
   }
 
+  setValues(user) {
+    if (!user) return
+
+    console.log(user)
+    Array.from(this.formEl.elements).forEach((field, index) => {
+
+      if (field.name === "gender") {
+        if (field.id === "exampleInputGenderM" && user.gender === "M" ) {
+          field.checked = true
+        }
+        else if (field.id === "exampleInputGenderF" && user.gender === "F" ) {
+          field.checked = true
+        }
+      }
+      else if (field.name === "admin") {
+        field.checked = user.admin
+      }
+      else if (field.name === "photo" && user.photo) {
+        // TODO
+      }
+      else {
+        field.value = user[field.name]
+      }
+    })
+
+  }
 
   onDelete() {
     const btDel = document.querySelectorAll("button.btn-delete")
@@ -57,9 +89,25 @@ class UserController {
   }
 
 
+  onEdit(trUser) {
+    const btnEdt = trUser.querySelectorAll("button.btn-edit")
+
+    this.formEl.reset()
+
+    if (btnEdt.length > 0) {
+      btnEdt[0].addEventListener("click", (event) => {
+        const td = event.currentTarget.parentElement
+        const tr = td.parentElement
+        const user = JSON.parse(tr.dataset.user)
+        this.userSel = user
+        this.userIndex = tr.rowIndex
+        this.setValues(user)
+      })
+    }
+  }
+
   onSubmit() {
     this.formEl.addEventListener("submit", (event) => {
-
       event.preventDefault()
 
       let user = this.getValues()
@@ -81,16 +129,28 @@ class UserController {
       this.getImage()
         .then((content) => {
           user.photo = content
-          this.addUser(user, this.tableId)
+
+          if (this.userSel.name) {
+            // this.editUser(user, this.tableId)
+          } else {
+            this.addUser(user, this.tableId)
+          }
+
           this.formEl.reset()
+          this.userSel = {}
           btn.disabled = false
         })
         .catch((err) => {
           console.error(err)
         })
+    })
 
+    this.formEl.addEventListener("reset", (event) => {
+      this.formEl.reset()
+      this.userSel = {}
     })
   }
+
 
   getImage() {
     return new Promise((resolve, reject) => {
@@ -117,11 +177,22 @@ class UserController {
     })
   }
 
+
   addUser(user) {
     let trUser = document.createElement('tr')
 
-    trUser.dataset.admin = user.admin
-    // trUser.dataset.user = JSON.stringify(user)
+    const objUser = {
+      name: user.name,
+      email: user.email,
+      gender: user.gender,
+      birth: user.birth,
+      country: user.country,
+      password: user.password,
+      photo: user.photo,
+      admin: user.admin
+    }
+
+    trUser.dataset.user = JSON.stringify(objUser)
 
     trUser.innerHTML = `
       <tr>
@@ -131,7 +202,7 @@ class UserController {
         <td>${user.admin ? "Sim" : "Não"}</td>
         <td>${Utils.dateFormat(user.dtcad)}</td>
         <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+          <button type="button" class="btn btn-primary btn-xs btn-flat btn-edit">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat btn-delete">Excluir</button>
         </td>
       </tr>
@@ -140,19 +211,64 @@ class UserController {
     this.tableEl.append(trUser)
     this.countUsers()
     this.onDelete()
+    this.onEdit(trUser)
   }
+
 
   countUsers() {
     let totUsers = 0
     let totAdmin = 0
 
     Array.from(this.tableEl.children).forEach((tr) => {
-      if (tr.dataset.admin === 'true') totAdmin ++
+      const user = JSON.parse(tr.dataset.user)
+      if (user.admin) totAdmin ++
       totUsers ++
     })
 
     document.querySelector("h3.tot-users").innerText = totUsers
     document.querySelector("h3.tot-admin").innerText = totAdmin
+  }
+
+  editUser(user) {
+    const trUsers = trUser.querySelectorAll('tr')
+    const trUser = trUsers[this.userIndex]
+    const oldUser = JSON.parse(trUser.dataset.user)
+
+    const objUser = {
+      name: user.name,
+      email: user.email,
+      gender: user.gender,
+      birth: user.birth,
+      country: user.country,
+      password: user.password,
+      photo: user.photo,
+      admin: user.admin
+    }
+
+    if (!user.photo && oldUser.photo) {
+      objUser.photo = oldUser.photo
+    }
+
+    trUser.dataset.user = JSON.stringify(objUser)
+
+    trUser.innerHTML = `
+      <tr>
+        <td><img src="${user.photo}" alt="User Image" class="img-circle img-sm"></td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.admin ? "Sim" : "Não"}</td>
+        <td>${Utils.dateFormat(user.dtcad)}</td>
+        <td>
+          <button type="button" class="btn btn-primary btn-xs btn-flat btn-edit">Editar</button>
+          <button type="button" class="btn btn-danger btn-xs btn-flat btn-delete">Excluir</button>
+        </td>
+      </tr>
+    `
+
+    this.tableEl.append(trUser)
+    this.countUsers()
+    this.onDelete()
+    this.onEdit(trUser)
   }
 
 }
